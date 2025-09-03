938df0c6e6f2978b37ec366fcb396a34d5d2ba76
김승상 <rovin054@gmail.com>
2025-09-03 15:00:41 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/basicboardv1/BasicBoardV1Application.java
@@ -0,0 +1,13 @@
+package com.example.basicboardv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBoardV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBoardV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicboardv1/BasicBoardV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicboardv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBoardV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
