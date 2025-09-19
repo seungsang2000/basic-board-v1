9068ce0845800274066aa1a3e07d5c5c3ec3efaf
김승상 <rovin054@gmail.com>
2025-09-19 12:09:26 +0900

    #9 : 경로 찾기 기능 구현

--- src/main/java/com/example/basicboardv1/map/controller/MapController.java
+++ src/main/java/com/example/basicboardv1/map/controller/MapController.java
@@ -1,11 +1,14 @@
 package com.example.basicboardv1.map.controller;
 
 import com.example.basicboardv1.map.config.KakaoMapConfig;
+import com.example.basicboardv1.map.dto.RouteRequest;
+import com.example.basicboardv1.map.dto.SimpleRouteResponse;
+import com.example.basicboardv1.map.service.KakaoRouteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/map")
@@ -13,10 +16,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MapController {
 
     private final KakaoMapConfig kakaoMapConfig;
+    private final KakaoRouteService kakaoRouteService;
 
     @GetMapping
     public String mapPage(Model model) {
         model.addAttribute("kakaoJavascriptKey", kakaoMapConfig.getJavascriptKey());
         return "map/map";
+    }
+
+    @PostMapping("/route")
+    @ResponseBody
+    public ResponseEntity<SimpleRouteResponse> findRoute(@RequestBody RouteRequest request) {
+        try {
+            SimpleRouteResponse route = kakaoRouteService.findRoute(request);
+            return ResponseEntity.ok(route);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
